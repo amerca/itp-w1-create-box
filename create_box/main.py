347d1708f7ddb176c06b739ecd916fc62732e3c2
amerca <amerca3@gmail.com>
2016-10-22 15:31:49 +0000

    boxes simple solution

--- create_box/main.py
+++ create_box/main.py
@@ -2,8 +2,15 @@
 
 
 def create_box(height, width, character):
-    pass
+    x=''
+    for i in range(0,height):
+        for j in range(0,width):
+            x+=character
+        else:
+            x+='\n'
+    return x
 
 
 if __name__ == '__main__':
     create_box(3, 4, '*')
+
